ba428caa98a2d202da07870a817914d50c5840bc
constantitus <constantitus@gmail.com>
2023-02-27 11:13:35 +0200

    test

--- src/Scenes/Player.cs
+++ src/Scenes/Player.cs
@@ -52,7 +52,7 @@ public partial class Player : CharacterBody2D
         for (int i = 0; i < GetSlideCollisionCount(); i++)
         {
             KinematicCollision2D collision = GetSlideCollision(i);
-            if ((collision.GetCollider() as Node).Name != "no")
+            if ((collision.GetCollider() as Node).Name != "no" && DedPlayer == false)
             {
                 DedPlayer = true;
                 Ded?.Invoke();
